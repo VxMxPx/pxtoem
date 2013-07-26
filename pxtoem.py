469d589cb4ffa365a57bf818ee8f6e732bc2392e
Marko Gajst <m@avrelia.com>
2013-07-26 23:42:07 +0200

    Fixed logic.

--- pxtoem.py
+++ pxtoem.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 def is_number(s):
     try:
         float(s)
@@ -7,30 +8,37 @@ def is_number(s):
     except ValueError:
         return False
 
-def do_convert(unit, conversion, conv_type):
-    if not is_number(unit): return 'Not a valid number!'
+def do_convert(unit, base, ignore_em):
+    conv_type = unit[-2:].lower()
+
+    if len(unit) < 3: return unit
+
+    unit = unit[:-2]
+
+    if not is_number(unit): return 'NaN'
+
     if conv_type == 'px':
-        return str("%.2f" % round(float(unit) / conversion, 2)).rstrip('0.').lstrip('0') + 'em'
+        return str("%.2f" % round(float(unit) / base, 2)).rstrip('0.').lstrip('0') + 'em'
+    elif not ignore_em:
+        return str("%.2f" % round(float(unit) * base, 2)).rstrip('0.').lstrip('0') + 'px'
     else:
-        return str("%.2f" % round(float(unit) * conversion, 2)).rstrip('0.').lstrip('0') + 'px'
+        return unit + conv_type
 
-base = input('Enter base value [npx | nem]: ')
-conv_type = base[-2:].lower()
-conversion = int(base[:-2])
 
-if conv_type != 'px' and conv_type != 'em':
-    print('Invalid value entered. Must end with px or em.')
-    exit()
+base = input('Enter base value in pixels: ')
+base = int(base)
+
+ignore_em = False if input('Ignore em inputs? [Y/n] ').lower() == 'n' else True
 
 while True:
-    unit = input(('em' if conv_type == 'px' else 'px') + '> ')
+    unit = input('> ')
     if unit.lower() in ['q', 'exit']: exit()
     # We're dealing with list of values!
     if not " " in unit:
-        print(do_convert(unit, conversion, conv_type))
+        print(do_convert(unit, base, ignore_em))
     else:
         units = unit.split(' ')
         stack = []
         for unit in units:
-            stack.append(do_convert(unit.strip(), conversion, conv_type))
+            stack.append(do_convert(unit.strip(), base, ignore_em))
         print(' '.join([str(item) for item in stack]))
